b1f445c4bfa02dc1d878e95599bc5d340afc4d5c
root <root@localhost.localdomain>
2024-12-08 19:04:23 +0800

    cmake and opensv

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "resize.h"
+#include <opencv2/opencv.hpp>
+#include <chrono>
+#include <iostream>
+#include <vector>
+#include <functional>
+using namespace std;
+int main(int argc, char** argv) {
+
+    cout<<"helloworld"<<endl;
+}
